ef6a44f92c929f36a1d79809abe46e9d85397fec
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-05-03 13:23:16 -0400

    setup

---
+++ Assets/laz-unity/scripts/Objects.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Strategies.Objects
+{
+	
+	public class Octree<TType>
+
+	public readonly struct Las
+	{
+		public readonly List<Vector3> points;
+		public readonly List<ushort> colors;
+		public readonly List<Byte> classifications;
+
+		public Las(List<Vector3> points, List<ushort> colors, List<byte> classifications)
+		{
+			this.points = points;
+			this.colors = colors;
+			this.classifications = classifications;
+		}
+
+		public override string ToString()
+		{
+			return$"Total Point Count: {points.Count}\nTotal Color Count: {colors.Count}\nTotal Class Count: {classifications.Count}";
+		}
+	}
+
+}
