14fd50bcef5cd39c2feaf972a695a057ddafdd32
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-05-04 13:54:50 -0400

    added potree wrappers

---
+++ Assets/laz-unity/scripts/Potree/Potree.Metadata.cs
@@ -0,0 +1,56 @@
+using System.IO;
+using System.Text;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Strategies.Objects
+{
+	public static partial class Potree
+	{
+
+		public static PotreeMetaData ReadJsonFromDir(string dir, bool moveToOrigin)
+		{
+			return ReadJsonFromPath(FormatJsonPath(dir), moveToOrigin);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="path"></param>
+		/// <param name="moveToOrigin"></param>
+		/// <returns></returns>
+		public static PotreeMetaData ReadJsonFromPath(string path, bool moveToOrigin)
+		{
+			var jsonfile = "";
+
+			using (StreamReader reader = new StreamReader(path, Encoding.Default))
+			{
+				jsonfile = reader.ReadToEnd();
+				reader.Close();
+			}
+
+			var data = JsonUtility.FromJson<PotreeMetaData>(jsonfile);
+
+			data.pointByteSize = 0;
+			foreach (var pointAttribute in data.attributes)
+				data.pointByteSize += pointAttribute.size;
+
+			return data;
+		}
+
+		public static string FormatJsonPath(string path)
+		{
+			if (!path.EndsWith("/"))
+				path += "/";
+
+			return path + MetaDataFormats.v2;
+		}
+
+		public static class MetaDataFormats
+		{
+			public const string v2 = "metadata.json";
+			public const string v1_8 = "cloud.js";
+		}
+
+	}
+}
